b09ad04a07cf9886175c181ab23bbdd538019542
mtcarpenter <dreamlixc@163.com>
2020-04-22 22:44:50 +0800

    nacos view code

---
+++ Finchley/sentinel-cloud-view-example/src/main/java/com/mtcarpenter/sentinel/cloud/example/TestController.java
@@ -0,0 +1,19 @@
+package com.mtcarpenter.sentinel.cloud.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 山间木匠
+ *
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/echo")
+    public String echo(){
+        return "mtcarpenter";
+    }
+}
---
+++ Finchley/sentinel-cloud-view-example/src/test/java/com/mtcarpenter/sentinel/cloud/example/SentinelCloudApplicationTests.java
@@ -0,0 +1,29 @@
+package com.mtcarpenter.sentinel.cloud.example;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.TimeUnit;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class SentinelCloudApplicationTests {
+
+    @Test
+    public void contextLoads() throws InterruptedException {
+        RestTemplate restTemplate = new RestTemplate();
+        for (int i = 50000; i > 0; i--) {
+            String forObject = restTemplate.getForObject("http://localhost:8081/test/echo/13", String.class);
+            log.info("result = {}", forObject);
+            TimeUnit.MILLISECONDS.sleep(600);
+        }
+
+    }
+
+}
